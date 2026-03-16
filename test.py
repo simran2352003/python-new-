e76b0ca9933f3b427b8f8068a8a3925b81c647a6
simran2352003 <dhimananu555@gmail.com>
2026-03-16 20:15:13 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+#numbers = [1,2,2,3,3,3,4,4,4,4]
+#frequency = {}
+#count frequency
+#for n in numbers:
+#    if n in frequency:
+#        frequency[n] = frequency[n]+1
+#    else:
+#        frequency[n] = 1
+#print("top 3 most frequency numbers:")
+#print("4 --", frequency[4], "times")
+#print("3--",  frequency[3], "times")
+#print("2--",  frequency[2], "times")
+
+#data = {
+#"aman": [85,90,78],
+#"ekam": [92,88,95],
+#"kiran":[70,75,80]
+#}      
+#avg = {}
+#for name in data:
+#    marks = data[name]
+#    average = sum(marks)/len(marks)
+#    avg[name] = average
+#    print(name, ":", round(average,1))
+#    top = max(avg, key=avg.get)
+#    print("top students:", top) 
